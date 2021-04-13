b232c44a98cd4437517872f10877991c10020815
Javier Leguina <javierleguina98@gmail.com>
2021-04-13 19:42:58 +0100

    Fixed demo

--- agents/PPOAgent.py
+++ agents/PPOAgent.py
@@ -1,13 +1,42 @@
-#!/usr/bin/env python
-
-import models
+#!/usr/bin/env python3
 
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torch.distributions as distributions
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class SharedCNN(nn.Module):
+    def __init__(self, action_dim=7):
+        super(SharedCNN, self).__init__()
+
+        self.image_conv = nn.Sequential(
+            nn.Conv2d(3, 16, (2, 2)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2)),
+            nn.Conv2d(16, 32, (2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, (2, 2)),
+            nn.ReLU())
+
+        self.policy_head = nn.Linear(64, action_dim)
+        self.value_head = nn.Linear(64, 1)
+        self.activation = nn.ReLU()
+
+    def forward(self, x, flag="policy"):
+        # Shared Body
+        x = x.view(-1, 3, 7, 7)  # x: (batch, C_in, H_in, W_in)
+        x = self.image_conv(x).squeeze()  # x: (batch, hidden)
+        # Split heads
+        if flag == "policy":
+            x_pol = self.policy_head(x)
+            return x_pol
+        elif flag == "value":
+            x_val = self.value_head(x)
+            return x_val
 
 
 class PPOAgent():
@@ -18,7 +47,7 @@ class PPOAgent():
         self.gamma = gamma
         self.lmbda = lmbda
 
-        self.model = models.SharedCNN().to(device)
+        self.model = SharedCNN().to(device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
 
         self.clip_param = clip_param
