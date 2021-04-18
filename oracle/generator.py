ad8e02646326b8c540825837e655ebf6560d86cb
Javier Leguina <javierleguina98@gmail.com>
2021-04-18 16:55:29 +0100

    Fully working Q&A; mostly wrong syntax tho

--- oracle/generator.py
+++ oracle/generator.py
@@ -1,14 +1,19 @@
 import nangram
 
-#import os
-#THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-#my_file = os.path.join(THIS_FOLDER, 'grammar.bnf')
-#grammar = nangram.Grammar.load_bnf(my_file)
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'grammar.bnf')
+grammar = nangram.Grammar.load_bnf(my_file)
 
-grammar = nangram.Grammar.load_bnf('grammar.bnf')
+# grammar = nangram.Grammar.load_bnf('grammar.bnf')
 
 grammar.max_products = 100000
 grammar.max_recursions = 100000
 grammar.max_repetitions = 100000
 
-gen_phrases = grammar.generate
+
+def gen_phrases():
+    premises = []
+    for expression in grammar.generate():
+        premises.append(expression)
+    return premises
