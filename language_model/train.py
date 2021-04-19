93cbfd2a08b4f1db48fcc7fe9c8b470c90554283
serg-e <smosesov@gmail.com>
2021-04-19 17:33:43 +0100

    Pre train language LSTM to produce valid premisesaccording grammar.bnf

---
+++ language_model/train.py
@@ -0,0 +1,45 @@
+import argparse
+import torch
+import numpy as np
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from .model import Model
+from .dataset import Dataset
+
+
+
+from dataclasses import dataclass
+
+def train(dataset, model, cfg):
+    model.train()
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=cfg.batch_size,
+    )
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    for epoch in range(cfg.epochs):
+        state_h, state_c = model.init_state(cfg.batch_size)
+
+        for batch, (x, y) in enumerate(dataloader):
+            if y.shape[0] < cfg.batch_size: continue
+
+            optimizer.zero_grad()
+
+            y_pred, (state_h, state_c) = model(x, (state_h, state_c))
+            loss = criterion(y_pred, y.view(-1))
+
+            state_h = state_h.detach()
+            state_c = state_c.detach()
+
+            loss.backward()
+            optimizer.step()
+
+            print({ 'epoch': epoch, 'batch': batch, 'loss': loss.item() })
+
+
+
+
---
+++ pretrain_lstm.py
@@ -0,0 +1,28 @@
+from language_model import train, Dataset, Model
+from oracle.generator import gen_phrases
+from typing import Callable
+from dataclasses import dataclass
+
+@dataclass
+class Config:
+    epochs: int = 20
+    batch_size : int = 256
+    sequence_len: int = 10
+    lstm_size : int = 128
+    word_embed_dims : int = 128
+    drop_out_prob : float = 0
+    gen_phrases : Callable = gen_phrases
+
+cfg = Config()
+dataset = Dataset(cfg)
+model = Model(dataset, cfg)
+train(dataset, model, cfg)
+
+
+for temp in [0.2, 0.5, 0.7, 1, 1.5]:
+    print("Predicting with temp: ", temp)
+    for i in range(10):
+        print(model.sample(temp))
+
+
+
