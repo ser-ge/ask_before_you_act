a2e18979d8fbc013f8f663fb6c3e76d69c3d08cf
Javier Leguina <javierleguina98@gmail.com>
2021-04-26 16:06:27 +0100

    Beautified

--- agents/Agent.py
+++ agents/Agent.py
@@ -11,29 +11,32 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Agent:
-    def __init__(self, model, learning_rate=0.001,
-                 gamma=0.99, clip_param=0.2, value_param=1, entropy_param=0.01,
-                 lmbda=0.95, backward_epochs=1):
+    def __init__(self, model, learning_rate=0.001, lmbda=0.95, gamma=0.99, clip_param=0.2,
+                 value_param=1, entropy_act_param=0.01, policy_qa_param=1, entropy_qa_param=0.05):
 
-        self.gamma = gamma
-        self.lmbda = lmbda
+        self.gamma = gamma  # Learning rate
+        self.lmbda = lmbda  # GAE multi-step bootstrap
 
+        # Loss weightings
+        self.clip_param = clip_param
+        self.entropy_act_param = entropy_act_param
+        self.value_param = value_param
+        self.entropy_qa_param = entropy_qa_param
+        self.policy_qa_param = policy_qa_param
+
+        # Policy softmax temperature
+        self.T = 1
+
+        # Model & optimizer
         self.model = model.to(device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
         self.optimizer_qa = optim.Adam(self.model.parameters(), lr=learning_rate)
 
-        self.clip_param = clip_param
-        self.entropy_param = entropy_param
-        self.value_param = value_param
-        self.T = 1
-
         self.done = True
         self.data = []
-        self.backward_epochs = backward_epochs
 
     def ask(self, observation):
-        # Will this be taking the history of questions?
-        # And history of states?
+        # TODO - Q&A history
         qa_history = torch.rand((1, 64))
         observation = torch.FloatTensor(observation).to(device)
         tokens, hx, log_probs_qa, entropy_qa = self.model.gen_question(observation, qa_history)
@@ -41,7 +44,6 @@ class Agent:
         return output, hx, log_probs_qa, entropy_qa
 
     def act(self, observation, ans, hx):
-        # Calculate policy
         observation = torch.FloatTensor(observation).to(device)
         ans = torch.FloatTensor(ans).view((-1, 2)).to(device)
         logits = self.model.policy(observation, ans, hx)
@@ -53,7 +55,7 @@ class Agent:
         return action.detach().item(), probs, entropy
 
     def update(self):
-        # torch.autograd.set_detect_anomaly(True)
+        # Get batch
         state, answer, word_lstm_hidden, action, reward, reward_qa, next_state, \
         log_prob_act, log_prob_qa, entropy_act, entropy_qa, done = self.get_batch()
 
@@ -62,30 +64,31 @@ class Agent:
         # Get next V
         next_V_pred = self.model.value(next_state, answer, word_lstm_hidden).squeeze()
 
-        mask = ~done # 1 if not done, 0 of done
         # Compute TD error
-        target = reward.squeeze().to(device) + self.gamma * next_V_pred * mask.squeeze().to(device)
+        # Moved this to buffer --> mask = ~done  # 1 if not done, 0 of done
+        target = reward.squeeze().to(device) + self.gamma * next_V_pred * done.squeeze().to(device)
         td_error = (target - V_pred).detach()
 
         # Generalised Advantage Estimation
         advantage = self.gae(td_error)
 
         # Clipped PPO Policy Loss
-        L_clip = self.clip_loss(action, advantage, answer,
-                                log_prob_act, state, word_lstm_hidden)
-
-        # Q&A Loss
-        L_qa = ((reward_qa + advantage.squeeze()) * log_prob_qa + 0.05 * entropy_qa).mean().to(device)
-        # L_qa = ((reward_qa) * log_prob_qa + 0.05 * entropy_qa).mean().to(device)
-
-        # Entropy regularizer
-        L_entropy = self.entropy_param * entropy_act.detach().mean()
+        L_clip = self.clip_loss(action, advantage, answer, log_prob_act, state, word_lstm_hidden)
 
         # Value function loss
         L_value = self.value_param * F.smooth_l1_loss(V_pred, target.detach())
 
+        # Entropy regularizer
+        L_entropy = self.entropy_act_param * entropy_act.detach().mean()
+
+        # Q&A Loss
+        L_qa_policy = self.policy_qa_param * (reward_qa + advantage.squeeze()) * log_prob_qa
+        L_qa_entropy = self.entropy_qa_param * entropy_qa
+        L_qa = (L_qa_policy + L_qa_entropy).mean().to(device)
+        # L_qa = ((reward_qa) * log_prob_qa + 0.05 * entropy_qa).mean().to(device)
+
+        # Final loss
         total_loss = -(L_clip + L_qa - L_value + L_entropy).to(device)
-        # total_loss = - (L_qa).to(device)
 
         # Update params
         self.optimizer.zero_grad()
@@ -123,15 +126,16 @@ class Agent:
         state = torch.FloatTensor(trans.state).to(device)
         answer = torch.FloatTensor(trans.answer).to(device)
         word_lstm_hidden = torch.cat(trans.word_lstm_hidden)
-        action = torch.FloatTensor(trans.action).to(device).view(-1,1)
-        reward = torch.FloatTensor(trans.reward).to(device).view(-1,1)
+        action = torch.FloatTensor(trans.action).to(device).view(-1, 1)
+        reward = torch.FloatTensor(trans.reward).to(device).view(-1, 1)
         reward_qa = torch.FloatTensor(trans.reward_qa).to(device)
         next_state = torch.FloatTensor(trans.next_state).to(device)
-        log_prob_act = torch.FloatTensor(trans.log_prob_act).to(device).view(-1,1)
+        log_prob_act = torch.FloatTensor(trans.log_prob_act).to(device).view(-1, 1)
         log_prob_qa = torch.stack(list(map(lambda t: torch.stack(t).mean().to(device), trans.log_prob_qa)))
-        entropy_act = torch.FloatTensor(trans.entropy_act).to(device).view(-1,1)
+        entropy_act = torch.FloatTensor(trans.entropy_act).to(device).view(-1, 1)
         entropy_qa = torch.FloatTensor(trans.entropy_qa).to(device)
-        done = torch.BoolTensor(trans.done).to(device).view(-1,1)
+        # Note: done has been negated to be able to mask the end-of-episode bootstrap step
+        done = torch.BoolTensor(~trans.done).to(device).view(-1, 1)
 
         self.data = []
 
