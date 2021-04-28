c56a70e849cd2ff29d9e4f5ff5dd52120f8c1740
rossmurphy <2120f8b9b5c26b33e4bb2778130aac68a675c021>
2021-04-28 16:19:34 +0100

    ross big change to baseline architecture
    BIG CHANGES

--- models/brain_netRM.py
+++ models/brain_netRM.py
@@ -11,7 +11,9 @@ class BrainNet(nn.Module):
     def __init__(self, question_rnn, action_dim=7, hidden_q_dim=128, mem_hidden_dim=64):
         super().__init__()
 
-        self.required_output_of_cnn_dim = hidden_q_dim - mem_hidden_dim
+        self.cnn_encoding_dim = hidden_q_dim - mem_hidden_dim
+        self.hidden_q_dim = hidden_q_dim
+        self.mem_hidden_dim = mem_hidden_dim
 
         self.image_conv = nn.Sequential(
             nn.Conv2d(3, 16, (2, 2)),
@@ -19,31 +21,31 @@ class BrainNet(nn.Module):
             nn.MaxPool2d((2, 2)),
             nn.Conv2d(16, 32, (2, 2)),
             nn.ReLU(),
-            nn.Conv2d(32, self.required_output_of_cnn_dim, (2, 2)),
+            nn.Conv2d(32, self.cnn_encoding_dim, (2, 2)),
             nn.ReLU())
 
-        self.mem_hidden_dim = mem_hidden_dim
-        self.eventual_input_dim = hidden_q_dim + 2 + mem_hidden_dim
-        self.policy_head = nn.Linear(self.eventual_input_dim, action_dim)  # 194 is 128 of hx + 2 of answer + 64 obs CNN
-        self.value_head = nn.Linear(self.eventual_input_dim , 1)  # 194 is 128 of hx + 2 of answer + 64 obs CNN
+        self.policy_input_dim =  self.cnn_encoding_dim + 2 + self.hidden_q_dim + self.mem_hidden_dim
+
+        self.policy_head = nn.Linear(self.policy_input_dim, action_dim)  # 194 is 128 of hx + 2 of answer + 64 obs CNN
+        self.value_head = nn.Linear(self.policy_input_dim, 1)  # 194 is 128 of hx + 2 of answer + 64 obs CNN
 
         # CNN output is 64 dims
         # Assuming qa_history is also 64
         self.question_rnn = question_rnn
         self.softmax = nn.Softmax(dim=-1)
 
-    def policy(self, obs, answer, hidden_q):
+    def policy(self, obs, answer, hidden_q, hidden_hist_mem):
         """
-        hidden_q : last hidden state
+        hidden_q : last hidden state§
         """
         encoded_obs = self.encode_obs(obs)
-        x = torch.cat((encoded_obs, answer, hidden_q), 1)
+        x = torch.cat((encoded_obs, answer, hidden_q, hidden_hist_mem), 1)
         action_policy = self.policy_head(x)
         return action_policy
 
-    def value(self, obs, answer, hidden_q):
+    def value(self, obs, answer, hidden_q,hidden_hist_mem):
         encoded_obs = self.encode_obs(obs)
-        x = torch.cat((encoded_obs, answer, hidden_q), 1)
+        x = torch.cat((encoded_obs, answer, hidden_q,hidden_hist_mem), 1)
         state_value = self.value_head(x)
         return state_value
 
@@ -78,7 +80,7 @@ class BrainNet(nn.Module):
 
     def encode_obs(self, obs):
         x = obs.view(-1, 3, 7, 7)  # x: (batch, C_in, H_in, W_in)
-        obs_encoding = self.image_conv(x).view(-1, self.required_output_of_cnn_dim)  # x: (batch, hidden)
+        obs_encoding = self.image_conv(x).view(-1, self.cnn_encoding_dim)  # x: (batch, hidden)
         return obs_encoding
 
 
@@ -86,9 +88,35 @@ class BrainNetMem(BrainNet):
 
     def __init__(self, question_rnn, action_dim=7):
         super().__init__(question_rnn, action_dim)
-        self.memory_rnn = nn.LSTMCell(self.eventual_input_dim+action_dim, self.mem_hidden_dim)
+        self.memory_rnn = nn.LSTMCell(self.cnn_encoding_dim+7+2+self.hidden_q_dim, self.mem_hidden_dim)
 
     def remember(self, obs, action, answer, hidden_q, memory):
         encoded_obs = self.encode_obs(obs)
         x = torch.cat((encoded_obs, action, answer, hidden_q), 1)
         return self.memory_rnn(x, memory)
+
+class BrainNetMemExpMem(BrainNetMem):
+    def __init__(self, question_rnn, action_dim=7):
+        super().__init__(question_rnn, action_dim)
+
+
+        self.mem_hidden_dim = mem_hidden_dim
+        self.eventual_input_dim = hidden_q_dim + 2 + mem_hidden_dim
+        self.policy_head = nn.Linear(self.eventual_input_dim, action_dim)  # 194 is 128 of hx + 2 of answer + 64 obs CNN
+        self.value_head = nn.Linear(self.eventual_input_dim, 1)  # 194 is 128 of hx + 2 of answer + 64 obs CNN
+
+
+    def policy(self, obs, answer, hidden_q):
+        """
+        hidden_q : last hidden state
+        """
+        encoded_obs = self.encode_obs(obs)
+        x = torch.cat((encoded_obs, answer, hidden_q), 1)
+        action_policy = self.policy_head(x)
+        return action_policy
+
+    def value(self, obs, answer, hidden_q):
+        encoded_obs = self.encode_obs(obs)
+        x = torch.cat((encoded_obs, answer, hidden_q), 1)
+        state_value = self.value_head(x)
+        return state_value
