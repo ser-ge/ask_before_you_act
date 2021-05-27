ab9cf49a517a9083c9596c17f4f6ac07efab8577
ser-ge <smosesov@gmail.com>
2021-05-27 15:50:50 +0000

    fixed baseline

--- utils/Trainer.py
+++ utils/Trainer.py
@@ -54,6 +54,7 @@ def train_test(env, agent, cfg, logger, n_episodes=1000,
             answer, reward_qa, entropy_qa = (1, 0, 1)
             log_prob_qa = 6 * [torch.Tensor([1])]
             hidden_q = torch.ones(128)
+            q_embedding = torch.ones(128)
 
         else:
             # Ask
