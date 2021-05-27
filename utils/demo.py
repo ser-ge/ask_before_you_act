098e975c7624799fbf424c1e14f8c5cc6b05f2f3
Thymo ter Doest <thymo@accountingbox.nl>
2021-05-27 13:58:46 +0100

    Chat bubbles in demo

--- utils/demo.py
+++ utils/demo.py
@@ -22,3 +22,207 @@ def wrap_env_video_monitor(env):
     env = Monitor(env, './video', force=True)
     return env
 
+import IPython
+from IPython.core.display import HTML
+from IPython.display import Javascript
+import json
+
+def show_qa(questions, answers):
+
+  display(HTML("""
+  <style>
+    video::-webkit-media-controls {
+    display: none;
+  }
+
+  .imessage {
+  border-radius: 0.25rem;
+  display: flex;
+  flex-direction: column;
+  font-family: "SanFrancisco";
+  font-size: 1.1rem;
+  /* margin: 0 auto 1rem; */
+  max-width: 230px;
+  padding: 0.5rem 1.5rem;
+}
+
+.imessage p {
+  border-radius: 1.15rem;
+  line-height: 1.25;
+  max-width: 75%;
+  padding: 0.5rem .875rem !important;
+  position: relative;
+  word-wrap: break-word;
+}
+
+.imessage p::before,
+.imessage p::after {
+  bottom: -0.1rem;
+  content: "";
+  height: 1rem;
+  position: absolute;
+}
+
+p.from-me {
+  align-self: flex-end;
+  background-color: #248bf5;
+  color: #fff; 
+}
+
+p.from-me::before {
+  border-bottom-left-radius: 0.8rem 0.7rem;
+  border-right: 1rem solid #248bf5;
+  right: -0.35rem;
+  transform: translate(0, -0.1rem);
+}
+
+p.from-me::after {
+  background-color: #383838;
+  border-bottom-left-radius: 0.5rem;
+  right: -40px;
+  transform:translate(-30px, -2px);
+  width: 10px;
+}
+
+p[class^="from-"] {
+  margin: 0.5rem 0;
+  width: fit-content;
+}
+
+p:first-letter{
+  text-transform: capitalize
+}
+
+p.from-me ~ p.from-me {
+  margin: 0.25rem 0 0;
+}
+
+p.from-me ~ p.from-me:not(:last-child) {
+  margin: 0.25rem 0 0;
+}
+
+p.from-me ~ p.from-me:last-child {
+  margin-bottom: 0.5rem;
+}
+
+p.from-them {
+  align-items: flex-start;
+  background-color: #e5e5ea;
+  color: #000;
+}
+
+p.from-them:before {
+  border-bottom-right-radius: 0.8rem 0.7rem;
+  border-left: 1rem solid #e5e5ea;
+  left: -0.35rem;
+  transform: translate(0, -0.1rem);
+}
+
+p.from-them::after {
+  background-color: #383838;
+  border-bottom-right-radius: 0.5rem;
+  left: 20px;
+  transform: translate(-30px, -2px);
+  width: 10px;
+}
+
+.no-tail::before {
+  display: none;
+}
+
+/* general styling */
+@font-face {
+  font-family: "SanFrancisco";
+  src:
+    url("https://cdn.rawgit.com/AllThingsSmitty/fonts/25983b71/SanFrancisco/sanfranciscodisplay-regular-webfont.woff2") format("woff2"),
+    url("https://cdn.rawgit.com/AllThingsSmitty/fonts/25983b71/SanFrancisco/sanfranciscodisplay-regular-webfont.woff") format("woff");
+}
+
+body {  
+  font-family: -apple-system, 
+    BlinkMacSystemFont, 
+    "Segoe UI", 
+    Roboto, 
+    Oxygen-Sans, 
+    Ubuntu, 
+    Cantarell, 
+    "Helvetica Neue", 
+    sans-serif;
+  font-weight: normal;
+  margin: 0;
+}
+
+@media screen and (max-width: 800px) {
+  body {
+    margin: 0 0.5rem;
+  }
+
+  .imessage {
+    font-size: 1.05rem;
+    margin: 0 auto 1rem;
+    max-width: 600px;
+    padding: 0.25rem 0.875rem;
+  }
+
+  .imessage p {
+    margin: 0.5rem 0;
+  }
+
+}
+
+.container {
+        width: 100%;
+        overflow-y: scroll;
+        /* padding-top: 20px;
+        padding-left: 40px; */
+        height: 400px;
+        scrollbar-color: black;
+      }
+  </style>
+  """))
+  display(Javascript('''
+
+  async function run() {
+    qa = (%s)
+    answers = qa[1]
+    questions = qa[0]
+    area = document.querySelector("#output-body .output_subarea")
+    area.style.display='flex'
+
+    while (true) {
+      let container = document.createElement("div");
+      let messages = document.createElement("div");
+      let empty = document.createElement("div");
+      area.appendChild(container);
+      container.appendChild(messages);
+      container.classList.add("container");
+      empty.style.height = '100px'
+      empty.style.width = '100%%'
+      container.appendChild(empty);
+
+      messages.classList.add("imessage")
+
+      for (let i = 0; i < answers.length; i++) {
+          let element = document.createElement("p");
+          // element.innerText = (i+1) + '. ' + questions[i] +'?'
+          element.innerText = questions[i] +'?'
+          element.className = "from-me"
+          messages.appendChild(element);
+
+          await new Promise(resolve => setTimeout(resolve, 300));
+
+          let element2 = document.createElement("p");
+          element2.innerText = answers[i]
+          element2.className = "from-them"
+          messages.appendChild(element2);
+          await new Promise(resolve => setTimeout(resolve, 700));
+
+          container.scrollTop = container.scrollHeight - container.clientHeight;
+      }
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      container.remove()
+    }
+  }
+  run()
+'''% json.dumps([questions, answers])))
+
